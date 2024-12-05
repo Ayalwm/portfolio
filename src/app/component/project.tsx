8b33b88137c3214c6558c8df9394c3babbb2e2c8
ayal <bdu1404571@bdu.edu.et>
2024-12-05 07:52:11 -0300

    added type to direction

--- src/app/component/project.tsx
+++ src/app/component/project.tsx
@@ -7,10 +7,10 @@ import { projects } from "./projects";
 export default function Projects() {
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  const handleChange = (direction) => {
-    setCurrentIndex((prevIndex) => 
-      direction === 'next' 
-        ? (prevIndex + 1) % projects.length 
+  const handleChange = (direction: 'next' | 'prev') => {
+    setCurrentIndex((prevIndex) =>
+      direction === 'next'
+        ? (prevIndex + 1) % projects.length
         : (prevIndex - 1 + projects.length) % projects.length
     );
   };
@@ -26,7 +26,8 @@ export default function Projects() {
           <h2 className="text-4xl font-extrabold text-center text-blue-400 mb-8">My Projects</h2>
 
           <div className="relative flex items-center justify-center">
-            <button className="absolute left-0 bg-blue-500 text-white rounded-full p-4 hover:bg-blue-600 z-30" onClick={() => handleChange('prev')}>◀</button>
+            {/* Previous Button */}
+            <button className="absolute left-0 bg-blue-500 text-white rounded-full p-4 hover:bg-blue-600 z-30 hidden md:block" onClick={() => handleChange('prev')}>◀</button>
 
             {/* Carousel Container */}
             <div className="flex justify-center items-center p-5 m-5 relative w-full h-[50vh] md:h-[60vh]">
@@ -36,8 +37,8 @@ export default function Projects() {
                 const isSide = position === "left" || position === "right";
 
                 return (
-                  <Link key={index} href={project.link} target="_blank" rel="noopener noreferrer" className={`absolute transform transition-all duration-700 ${isCenter ? "z-30 scale-120 top-0 hover:scale-105" : isSide ? "z-20 scale-90 top-10" : "hidden"} ${position === "left" ? "translate-x-[-120%]" : ""} ${position === "right" ? "translate-x-[120%]" : ""}`} style={{ transformStyle: "preserve-3d" }}>
-                    <div className="w-[50vw] h-[70vh] md:w-[30vw] md:h-[50hv] bg-gray-800 rounded-lg overflow-hidden shadow-xl relative">
+                  <Link key={index} href={project.link} target="_blank" rel="noopener noreferrer" className={`absolute transform transition-all duration-700 ${isCenter ? "z-30 scale-120 top-0 hover:scale-105 w-full md:w-[50vw]" : isSide ? "z-20 scale-90 top-10" : "hidden"} ${position === "left" ? "translate-x-[-120%]" : ""} ${position === "right" ? "translate-x-[120%]" : ""}`} style={{ transformStyle: "preserve-3d" }}>
+                    <div className="w-full md:w-[50vw] h-[70vh] bg-gray-800 rounded-lg overflow-hidden shadow-xl relative">
                       <span className="absolute top-4 right-4 bg-gray-800 text-white text-xs font-semibold rounded-full px-3 py-1 shadow-md">{project.language}</span>
                       {project.image && <Image src={project.image} alt={`${project.name} thumbnail`} width={350} height={250} className="w-full h-3/5 object-cover rounded-t-lg" />}
                       <div className="p-4 h-2/5 overflow-auto no-scrollbar">
@@ -50,7 +51,8 @@ export default function Projects() {
               })}
             </div>
 
-            <button className="absolute right-0 bg-blue-500 text-white rounded-full p-4 hover:bg-blue-600 z-30" onClick={() => handleChange('next')}>▶</button>
+            {/* Next Button */}
+            <button className="absolute right-0 bg-blue-500 text-white rounded-full p-4 hover:bg-blue-600 z-30 hidden md:block" onClick={() => handleChange('next')}>▶</button>
           </div>
         </div>
       </section>
