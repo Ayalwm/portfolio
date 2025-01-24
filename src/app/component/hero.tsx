b69557e29d86950ba8def63270d1abf8f7d64ef1
ayal <bdu1404571@bdu.edu.et>
2025-01-24 04:33:50 -0300

    link in a new tab

--- src/app/component/hero.tsx
+++ src/app/component/hero.tsx
@@ -31,6 +31,7 @@ export default function Hero() {
               </Link>
               <Link
                 href="https://www.canva.com/design/DAGX9ynHlaI/2JfciXLDi1-t5weUQcXSdg/edit?utm_content=DAGX9ynHlaI&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton"
+                target="_blank"
                 className="ml-3 bg-blue-500 text-white text-lg font-bold px-6 rounded-full hover:bg-blue-700 transition-all"
               >
                 Resume
